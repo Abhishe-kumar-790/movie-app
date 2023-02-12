3fe7113bd444bbd1cd61c7d2617fcc9ae44e2a19
Abhishe-kumar-790 <101246320+Abhishe-kumar-790@users.noreply.github.com>
2023-02-13 02:58:34 +0530

    fix ui

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -11,7 +11,7 @@ export default function Movies() {
                         movie.map((item, index) => {
                             const { id, title, poster_path } = item;
                             return <div className="col-md-3 g-3" key={index}>
-                                <div className="card" style={{ width: '18rem', backgroundColor: '#000' }}>
+                                <div className="card" style={{ backgroundColor: '#000' }}>
                                     <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} className="card-img-top" alt="..." height="250" />
                                     <div className="card-body">
                                         <p className="card-title fw-bold">{(title.length >= 15) ? `${title.substring(0, 25)} ...` : title}</p>
